5d1d0fb04094ce46a0e1517438148a6aec3a7a3d
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-05-10 11:43:15 +0500

    feat: added reusable button and link components

---
+++ utils/components/button/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return <button role='button' className='bg-sky-700 mt-4 px-[30px] py-[10px] cursor-pointer hover:bg-sky-600' {...props}>
+    {
+      props.children
+    }
+  </button>
+}
+
+export default Button
