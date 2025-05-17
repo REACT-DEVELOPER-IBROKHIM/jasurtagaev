39bef9a6bad984c0aeac59d2aced76378da7cc7b
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-05-18 01:11:56 +0500

    fix: rearranged the layout of the sections

---
+++ app/articles/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header";
+import Main from "@/components/main";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default page;
