9017dd0f6af2304faa4bcbb22f7cc482a84879fe
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-05-18 01:29:35 +0500

    fix: deployment error with fetch

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,27 +1,25 @@
 import React from "react";
-import type { Metadata, ResolvingMetadata } from "next";
+// import type { Metadata, ResolvingMetadata } from "next";
 import { Props } from "./type";
-import { API_URL } from "@/contants/api";
+// import { API_URL } from "@/contants/api";
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  const slug = (await params).id;
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata,
+// ): Promise<Metadata> {
+//   const slug = (await params).id;
 
-  const post = await fetch(`${API_URL}/${slug}`).then((res) =>
-    res.json(),
-  );
+//   const post = await fetch(`${API_URL}/${slug}`).then((res) =>
+//     res.json(),
+//   );
 
-  return {
-    title: post.title,
-    description: post.description,
-  };
-}
+//   return {
+//     title: post.title,
+//     description: post.description,
+//   };
+// }
 
 const page = async ({ params }: Props) => {
-  const { id } = await params;
-  console.log(id);
   return <div>page</div>;
 };
 
