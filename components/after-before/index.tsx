2b4228833f79798e033f35dc61a0e404662c91ef
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2025-07-18 23:30:08 +0500

    fix: some media issues

--- components/after-before/index.tsx
+++ components/after-before/index.tsx
@@ -1,10 +1,5 @@
-"use client";
 import { IAfterBefore } from "@/types/article";
-import ExpandLink from "@/utils/components/expand";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
-import React from "react";
-import { useTranslations } from "use-intl";
+import AfterBeforeCard from "../after-before-card";
 
 const AfterBefore = ({
   afterBefore,
@@ -13,9 +8,6 @@ const AfterBefore = ({
   afterBefore: IAfterBefore[];
   isFeatured?: boolean;
 }) => {
-  const t = useTranslations("after_before");
-  const th = useTranslations("home");
-  const pathname = usePathname();
   return (
     <>
       <section className="w-full mb-[50px]">
@@ -25,38 +17,10 @@ const AfterBefore = ({
               isFeatured ? media.isFeatured === isFeatured : true,
             )
             .map((item) => (
-              <div key={item.id} className="mb-[30px]">
-                <h3 className="text-2xl mb-2">{t("title")}</h3>
-                <div className="gap-4 flex flex-col items-center md:flex-row">
-                  <div className="w-full md:w-1/2">
-                    <Image
-                      src={item.urls[0]}
-                      alt="Before"
-                      width={500}
-                      height={300}
-                      className="w-full h-[400px] object-cover"
-                    />
-                  </div>
-                  <div className="w-full md:w-1/2">
-                    <h3 className="text-2xl mb-2"></h3>
-                    <Image
-                      src={item.urls[1]}
-                      alt="After"
-                      width={500}
-                      height={300}
-                      className="w-full h-[400px] object-cover"
-                    />
-                  </div>
-                </div>
-              </div>
+              <AfterBeforeCard key={item.id} item={item} />
             ))}
         </div>
       </section>
-      {!pathname.includes("media") && (
-        <div className="flex justify-center mb-[50px]">
-          <ExpandLink href="/media">{th("reusable.explore_more")}</ExpandLink>
-        </div>
-      )}
     </>
   );
 };
--- components/media-card/index.tsx
+++ components/media-card/index.tsx
@@ -18,10 +18,20 @@ const MediaCard = ({
       key={media.id}
       className="flex-1 bg-gradient-to-r from-white/60 to-[#082D4F]/70 bg-[#1e73be] text-white text-center"
     >
-      {<img width={"100%"} height={"400px"} src={media.thumbnail} alt="" />}
-      <div className="p-6 py-10">
-        <p className="text-2xl mb-8">{media.tag[locale]}</p>
-        <h2 className="text-[18px] mb-4 min-h-[60px]">{media.title[locale]}</h2>
+      {
+        <img
+          className="w-full h-[200px] lg:h-[300px] object-cover"
+          width={"100%"}
+          height={"400px"}
+          src={media.thumbnail}
+          alt=""
+        />
+      }
+      <div className="p-2 lg:p-4 py-10">
+        <p className="text-xl mb-4 lg:text-2xl lg:mb-8">{media.tag[locale]}</p>
+        <h2 className="text-[16px] lg:text-[18px] mb-4 min-h-[60px] h-[70px]">
+          {media.title[locale]}
+        </h2>
         <Button
           onClick={() => {
             setSelectedMedia(media);
